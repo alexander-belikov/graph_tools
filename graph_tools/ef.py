471734e110c775fb81c61b3ffd15a8c39de32051
Alexander Belikov <abelikov@gmail.com>
2017-02-02 14:18:38 -0600

    first commit; ef function

---
+++ graph_tools/ef.py
@@ -0,0 +1,52 @@
+from numpy import fill_diagonal, zeros, ones, repeat
+from numpy import where, dot, sum
+
+
+def calc_eigen_vec(z, a, alpha=0.85, eps=1e-6):
+    # check that z is square, the shapes of a and z are compatible
+    n = z.shape[0]
+    fill_diagonal(z, 0)
+    z_j = sum(z, axis=0)
+
+    dangle_vec = zeros(shape=n)
+    dangle_mask = (z_j == 0.)
+    dangle_vec[dangle_mask] = 1.0
+
+    # avoid dividing by zero
+    z_j_prime = z_j.copy()
+    z_j_prime[z_j_prime == 0.] = 1.0
+
+    h = z / z_j_prime
+    pi_0 = ones(shape=z_j.shape) / n
+    h_prime = h.copy()
+
+    mm = repeat(dangle_mask.reshape((1, n)), n, axis=0)
+    aa = repeat(a.reshape((n, 1)), n, axis=1)
+    h_prime = where(mm, aa, h)
+
+    p = alpha * h_prime + (1 - alpha) * aa
+
+    pi = pi_0.copy()
+    pi_next = pi_0.copy()
+
+    norm = 1.0
+    k = 0
+    while norm > eps:
+        pi_next = alpha * dot(h, pi) + (alpha * dot(pi, dangle_vec) + (1 - alpha) * sum(pi)) * a
+        norm = sum((pi_next - pi) ** 2) ** 0.5
+        pi = pi_next
+        #         print k, norm
+        k += 1
+        if norm > 1e1:
+            print 'norm is too high'
+            break
+    ef_aux = dot(h, pi)
+    ef = 1e2 * ef_aux / sum(ef_aux)
+    # avoid zero by zero div
+    # in practice it's never the case
+    a_prime = a.copy()
+    zero_mask = (a == 0.)
+    a_prime[zero_mask] = 1.0
+    af = 0.01 * ef / a_prime
+    #     return h_prime, dangle_mask, mm, h, aa, dangle_vec, pi, pi_0
+    return ef, af
---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="graph_tools",
+    version="0.0.1",
+    author="Alexander Belikov",
+    author_email="abelikov@gmail.com",
+    description="tools reducing bipartite graphs and applying ranking algorithms",
+    license="BSD",
+    keywords="eigenfactor",
+    # url="git@github.com:alexander-belikov/datahelpers.git",
+    packages=['graph_tools'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 0 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires=[
+        'numpy'
+    ],
+)
