d5d32393be813d84d000b752fe5b9a07ea4ed04a
Alexander Belikov <abelikov@gmail.com>
2017-02-28 14:38:54 -0600

    fix reduce

--- graph_tools/reduce.py
+++ graph_tools/reduce.py
@@ -1,6 +1,6 @@
 from networkx import DiGraph, Graph
 from numpy import array, dot
-
+import logging
 
 def reduce_digraphs(ga, gb):
     """
@@ -41,28 +41,33 @@ def reduce_bigraphs(ga, gb, out_labels=None, ga_labels=None, gb_labels=None):
         ga_types = set([item[0] for item in ga.nodes()])
         gb_types = set([item[0] for item in gb.nodes()])
     except:
-        raise(' in reduce_bigraphs() : ga or gb are not typified '
+        logging.error(' in reduce_bigraphs() : ga or gb are not typified '
               '(nodes should be tuples, e.g. (type, value))')
+        raise
 
     if len(ga_types) != 2 or len(gb_types) != 2:
-        raise(' in reduce_bigraphs() : one of the graphs '
+        logging.error(' in reduce_bigraphs() : one of the graphs '
               'is not bipartite')
+        raise
 
-    if not ga_labels and gb_labels:
+    if not ga_labels and not gb_labels:
         try:
             r = list(ga_types & gb_types)[0]
             a = list(ga_types - gb_types)[0]
             b = list(gb_types - ga_types)[0]
         except:
-            raise(' in reduce_bigraphs() : ga and gb '
+            logging.error(' in reduce_bigraphs() : ga and gb '
                   'do not share a type')
+            raise
 
     if out_labels:
         out_left, out_right = out_labels
+    elif ga_labels and gb_labels:
+        out_left, out_right = ga_labels[0], gb_labels[0]
     else:
         out_left, out_right = a, b
 
-    if ga_labels and ga_labels == (a, r):
+    if ga_labels:
         ga_left, ga_right = ga_labels
     else:
         ga_left, ga_right = (a, r)
@@ -80,12 +85,14 @@ def reduce_bigraphs(ga, gb, out_labels=None, ga_labels=None, gb_labels=None):
 
     for a in a_left:
         r_ga = ga.neighbors(a)
+        r_ga_ = list(map(lambda x: x[1], r_ga))
         for b in b_left:
             r_gb = gb.neighbors(b)
-            r_intersection = list(set(r_ga) & set(r_gb))
+            r_gb_ = list(map(lambda x: x[1], r_gb))
+            r_intersection = list(set(r_ga_) & set(r_gb_))
             if r_intersection:
-                ar_vec = array([ga[a][r]['weight'] for r in r_intersection])
-                br_vec = array([gb[b][r]['weight'] for r in r_intersection])
+                ar_vec = array([ga[a][(ga_right, r_)]['weight'] for r_ in r_intersection])
+                br_vec = array([gb[b][(gb_right, r_)]['weight'] for r_ in r_intersection])
                 a_ = (out_left, a[1])
                 b_ = (out_right, b[1])
                 gout.add_edge(a_, b_, {'weight': dot(ar_vec, br_vec)})
